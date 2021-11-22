6d4ef44fb5751ba82e196925d4fb022e48bf343a
angelinajosephine <91074692+angelinajosephine@users.noreply.github.com>
2021-11-22 19:36:42 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,18 @@
+var score = 0;
+
+function updatescore()
+{
+    score = score+1;
+    document.getElementById("score").innerHTML = "score:" +score;
+}
+
+function savescore()
+{
+    localStorage.setItem("score",score);
+
+}
+
+function nextpage()
+{
+    window.location = "https://www.youtube.com/";
+}
